a12687ddbdc2b03a599233418ec32bf2ef7f09b3
Neel Gujarathi <neelgujarathi66@gmail.com>
2026-01-13 12:05:17 +0530

    add backend link

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-  withCredentials: true
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "https://gig-platform-backend.onrender.com/api",
+  withCredentials: true,
 });
-
-export default API;
